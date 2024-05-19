2df61db73db178f842c60fb286623dbfd2ecb786
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-05-19 18:55:58 +0300

    files added

---
+++ DI_Bootcamp_stage_1/week_3/day1/classExercises/script.js
@@ -0,0 +1,55 @@
+
+
+// exercise 1
+
+// const addressNumber = "22";
+// const addressStreet = "Hillel";
+// const country = "Isreal";
+
+// var globalAddress = `I live on ${addressStreet} ${addressNumber} street, ${country}.`;
+
+
+// console.log(globalAddress);
+
+// Execise 2
+
+// var birthYear = 1997;
+// var futureYear = 2033;
+// var calculatedYear = futureYear - birthYear;
+
+// var sentence = `I will be ${calculatedYear} years old in the year ${futureYear}.`;
+
+// console.log(sentence);
+
+// exercise 3
+
+const Pets =  ['cat','dog','fish','rabbit','cow'];
+
+Pets.splice(3, 1, 'horse');
+
+
+console.log(Pets);
+
+
+// console.log(newPets.length);
+
+
+
+
+// Splice for replacement 
+// Splice syntax: splice(indexPosition, replacementValue)
+// example: list  ['cat','dog','fish','rabbit','cow']
+// list.splice(1, 2, snake, eel)
+// list =  ['cat','snake','eel','rabbit','cow']
+
+// operators
+// ? look up: Nullish coelescing operator  sign = [??]
+
+
+
+
+
+
+
+
+
