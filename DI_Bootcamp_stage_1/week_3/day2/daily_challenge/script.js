1b770277d1ef9124a102f0ed5ba6a0244df7139c
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-05-19 18:55:06 +0300

    week3_day2 Daily challenge

---
+++ DI_Bootcamp_stage_1/week_3/day2/daily_challenge/script.js
@@ -0,0 +1,25 @@
+// exercise 1
+
+// let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+// fruits.splice(0,1);
+
+
+// fruits.push("kiwi");
+
+// Pops the first element.
+// fruits.shift();
+
+// fruits.sort();
+
+// fruits.reverse();
+
+// console.log(fruits);
+
+// exercise 2
+
+// const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+// moreFruits[1][1][0];
+
+// console.log(moreFruits[1][1][0]);
