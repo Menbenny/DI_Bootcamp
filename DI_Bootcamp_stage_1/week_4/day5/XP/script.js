4e23b30ff706c8154d150abfb1e2ee5687c57862
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-05-28 22:05:24 +0300

    Project Xp exercise

---
+++ DI_Bootcamp_stage_1/week_4/day5/XP/script.js
@@ -0,0 +1,48 @@
+
+
+// const playTheGame = () => {
+//     let ask = confirm(`Do you want to start the game? `);
+//     if (ask === false) {
+//         alert(`No problem, Goodbye`);
+//     } else {
+//         var userNumber = Number(prompt(`Enter a number between 0 - 10: `))
+        
+//     }
+//         if (isNaN(userNumber)) {
+//             alert(`Sorry not a number, Goodbye. `);
+//         } else if (0 > userNumber || userNumber > 10) {
+//             alert(`Sorry it's not a good number, Goodbye. `);
+//         } else {
+//             computerNumber = Math.floor(Math.random() * 10);
+//             alert(`Your number: ${userNumber}
+//             \nComputer's number: ${computerNumber}`)
+//         }
+    
+// }
+
+
+
+// playTheGame();
+
+// const compareNumbers = (userNumber,computerNumber) => {
+    
+    
+//     if (userNumber === computerNumber) {
+//         alert(`WINNER`);
+        
+//     } else for (let i = 0; i < 4; i++) {
+//             if ( userNumber > computerNumber) {
+//                 alert(`Your number is bigger then the computer's`)
+//             } else (userNumber < computerNumber) ;{
+//                 alert(`Your number is smaller then the computer's`)
+//             }
+//         }
+        
+       
+    
+
+
+//     console.log(`${computerNumber}  ${userNumber}`);
+// }
+
+// compareNumbers(playTheGame());
