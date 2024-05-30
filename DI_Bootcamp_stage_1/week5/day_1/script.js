c35dc7e8cca079a50471f6a9bda71243fe058165
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-05-30 19:42:41 +0300

    form manipulation

---
+++ DI_Bootcamp_stage_1/week5/day_1/script.js
@@ -0,0 +1,20 @@
+// var getvalue = (e) => {
+    // var first_name = document.querySelector();
+    // var last_name = document.querySelector("label");
+
+    // console.log(first_name, last_name);
+
+//     console.log(e.target);
+
+//     document.getElementById("form1").addEventListener("submit", function(event)){
+//         event.target.input.fname.value
+//     }
+
+// }
+
+document.getElementById("form1").addEv
+document.getElementById("form1").addEventListener("submit", function(event){
+    alert((event.target.fname.value), (event.target.lname.value));
+ });
+
+// getvalue()
