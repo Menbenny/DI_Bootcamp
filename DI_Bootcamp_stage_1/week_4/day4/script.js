ffaf13f7afa1570590e0d2ed47d55208896ceded
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-04 17:56:03 +0300

    week_4 day4

---
+++ DI_Bootcamp_stage_1/week_4/day4/script.js
@@ -0,0 +1,22 @@
+// var container = document.querySelector('div')
+
+// console.log(container);
+
+var pete = document.getElementsByClassName('list')
+
+// var namePete = pete[0].lastElementChild.innerHTML
+// var changeName = namePete.replace('Richard')
+// var changeName = pete[0].lastElementChild.textContent
+// var changeName = 'Richard'
+
+// console.log(changeName);
+
+var parent = document.getElementsByClassName('list')
+var Sarah = document.querySelector('ul')[1].children[1].innerHTML
+
+parent.removeChild(Sarah)
+
+
+
+
+// console.log(Sarah);
