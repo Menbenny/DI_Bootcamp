8c20b2322e4c57d94e9eb14f256663ca06784746
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-05 19:24:16 +0300

    week 4 day 4 exercise 2

--- DI_Bootcamp_stage_1/week_4/day4/script.js
+++ DI_Bootcamp_stage_1/week_4/day4/script.js
@@ -12,18 +12,35 @@
 // console.log(changeName);
 
 
-//3
-const lists = document.querySelectorAll('.list')
-lists[1].children[1].remove()
+// //3
+// const lists = document.querySelectorAll('.list')
+// lists[1].children[1].remove()
 
-//4
-lists.forEach(list => list.firstElementChild.textContent = 'ben')
+// //4
+// lists.forEach(list => list.firstElementChild.textContent = 'ben')
 
-//Bonus
-lists.forEach(list => list.classList.add('students_list'))
+// //Bonus
+// lists.forEach(list => list.classList.add('students_list'))
 
-document.querySelector('ul').classList.add('university', 'attendence')
+// document.querySelector('ul').classList.add('university', 'attendence')
 
+// EXERCISE 2
 
+const div = document.querySelector('div')
+
+div.style.backgroundColor = 'lightblue'
+div.style.padding = '10px'
+
+const Pete = document.querySelector('ul').children[1]
+
+Pete.style.border = '1px solid black'
+const body = document.body
+body.style.fontSize = '2em'
+
+while (div.style.background === 'lightblue') {
+    let x = Pete;
+    let y = document.querySelector('ul').children[0].innerHTML;
+    alert(`Hello ${x} and ${y}`)
+}
 
 
