024a37e127f9bff1fc76891a66e32ef64418df73
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-05 19:13:54 +0300

    week 4 day 4 exercise 1

--- DI_Bootcamp_stage_1/week_4/day4/script.js
+++ DI_Bootcamp_stage_1/week_4/day4/script.js
@@ -1,22 +1,29 @@
+//1
 // var container = document.querySelector('div')
-
 // console.log(container);
 
-var pete = document.getElementsByClassName('list')
 
-// var namePete = pete[0].lastElementChild.innerHTML
-// var changeName = namePete.replace('Richard')
+
+//2
+// var pete = document.getElementsByClassName('list')
 // var changeName = pete[0].lastElementChild.textContent
 // var changeName = 'Richard'
 
 // console.log(changeName);
 
-var parent = document.getElementsByClassName('list')
-var Sarah = document.querySelector('ul')[1].children[1].innerHTML
 
-parent.removeChild(Sarah)
+//3
+const lists = document.querySelectorAll('.list')
+lists[1].children[1].remove()
+
+//4
+lists.forEach(list => list.firstElementChild.textContent = 'ben')
+
+//Bonus
+lists.forEach(list => list.classList.add('students_list'))
+
+document.querySelector('ul').classList.add('university', 'attendence')
 
 
 
 
-// console.log(Sarah);
