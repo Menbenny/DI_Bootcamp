2e9c977dcd5b6f41284056e8f00dd5bd31665938
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-21 18:43:02 +0300

    Week4 - bottles of beers redone

--- DI_Bootcamp_stage_1/week_4/day5/Daily_Challenge/script.js
+++ DI_Bootcamp_stage_1/week_4/day5/Daily_Challenge/script.js
@@ -1,18 +1,48 @@
-const numberOfBeers = prompt(`How many beers? `)
+let numberOfBeers = getUserInput();
+let numberOfBeersToTakeAway = 1;
 
-const lyric = makeLyric();
-makeLyric = (num) => {
+//     // const lyric = makeLyric();
 
-    console.log(`Is this number more than one ${isPlural(num)}`);
-    `${num} bottles of beer on the wall
-    ${num} bottles of beer
-    Take 1 down, pass it around
-    ${num - 1} bottles of beer on the wall`;
+getUserInput = () => {
+    const answer = prompt(`How many beers?`)
+    if (answer == '') return alert(`Please enter something`);
+    const number = Number(answer);
+    if (Number.isNaN(number)) return alert(`Please enter a number`);
+    return number
+}
+
+while (numberOfBeers > 0) {
+    const lyric = makeLyric(numberOfBeers, numberOfBeersToTakeAway);
+    console.log(lyric);
+    numberOfBeers -= numberOfBeersToTakeAway;
+    numberOfBeersToTakeAway++;
+}
+
+makeLyric = (num, counter) => {
+    const bottleOrBottles = getBottleOrBottles(num);
+    const bottlesToTakeAway = getSmallerNumber(counter, num);
+    const remainingBottles = getBiggerNumber(num - counter, 0);
+    const remainingBottleOrBottles = getBottleOrBottles(remainingBottles)
+
+    const lyric = `${numberOfBeers} ${bottleOrBottles} of beer on the wall ${numberOfBeers} bottles of beer Take ${bottlesToTakeAway} down, pass it around ${remainingBottles} ${remainingBottleOrBottles} of beer on the wall`;
 
     return lyric
 }
 
-isPlural = () => {
-    return (num > 1)
+getBiggrtNumber = (a, b) => {
+    return a > b ? a : b;
 }
-makeLyric()
+
+getSmallerNumber = (a, b) => {
+    return a < b ? a : b;
+}
+
+isPlural = () => {
+  return num > 1;
+}
+
+getBottleOrBottles = (num) => {
+    return isPlural(num) ? "bottles" : "bottle"
+    }
+    
+
