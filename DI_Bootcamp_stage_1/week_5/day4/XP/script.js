9fd9e144eeb99a158b051547806b2b978e976d02
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-22 18:31:15 +0300

    week5 day4 DOM animation exercises redone

--- DI_Bootcamp_stage_1/week_5/day4/XP/script.js
+++ DI_Bootcamp_stage_1/week_5/day4/XP/script.js
@@ -1,38 +1,38 @@
 // Exercise 1
 
-hello = () => {
-    //  Part 1
-    alert(`Hello World`)
-}
+// hello = () => {
+//     //  Part 1
+//     alert(`Hello World`)
+// }
 
-    // ! Part 2
-addParagraph = () => {
-    const container = document.getElementById('container')
-    const hello = document.createElement('p')
-    hello.textContent = 'Hello there!'
-    container?.children.length <  5 ? container?.appendChild(hello) : clearParagraphInterval()
+//     // ! Part 2
+// addParagraph = () => {
+//     const container = document.getElementById('container')
+//     const hello = document.createElement('p')
+//     hello.textContent = 'Hello there!'
+//     container?.children.length <  5 ? container?.appendChild(hello) : clearParagraphInterval()
     
-}
+// }
 
-setTimeout(hello, 2000)
-setTimeout(addParagraph, 2000)
-// ! Part 3
-// onclick HTML 
+// setTimeout(hello, 2000)
+// setTimeout(addParagraph, 2000)
+// // ! Part 3
+// // onclick HTML 
 
-const int = setInterval(addParagraph, 2000)
-const button = document.getElementById('clear')
-button?.addEventListener("click", clearParagraphInterval)
+// const int = setInterval(addParagraph, 2000)
+// const button = document.getElementById('clear')
+// button?.addEventListener("click", clearParagraphInterval)
 
 
-clearParagraphInterval = () => {
-    clearInterval(int)
-}
-// ! Clear Interval not working
-// ? Check why
+// clearParagraphInterval = () => {
+//     clearInterval(int)
+// }
+// // ! Clear Interval not working
+// // ? Check why
 
 // Exercise 2
 // let spaces = 0
-// interval;
+// let interval;
 
 // myMove = () => {
 //     interval = setInterval(moveBox, 1)
@@ -49,8 +49,25 @@ clearParagraphInterval = () => {
 //     }
 // }
 
-// const container = document.getElementById('container')
+// exercise 3
+const yellowBox = document.getElementById("target");
+const redBox = document.getElementById("box");
 
+dragStart = () => {
+    console.log(`dragstart`);
+}
+
+allowDrop = (event) => {
+    event.preventDefault();
+    console.log(event);
+    yellowBox.classList.add("is-hovered");
+}
+
+handleDrop = (event) => {
+    console.log(handleDrop);
+    yellowBox.classList.remove("is-hovered")
+    yellowBox.append(box)
+}
 
 
 
