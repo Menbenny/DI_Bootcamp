9f8603361707b85e4256038f7637b99ed8971383
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-22 13:47:21 +0300

    week5 day4 DOM animation exercises redone

--- DI_Bootcamp_stage_1/week_5/day4/XP/script.js
+++ DI_Bootcamp_stage_1/week_5/day4/XP/script.js
@@ -1,55 +1,55 @@
 // Exercise 1
 
-// hello = () => {
-    // ! Part 1
-    // alert(`Hello World`)
+hello = () => {
+    //  Part 1
+    alert(`Hello World`)
+}
 
     // ! Part 2
-    // const container = document.getElementById('container')
-    // const hello = document.createElement('p')
-    // hello.textContent = 'I love Gabby'
-    // container.appendChild(hello)
+addParagraph = () => {
+    const container = document.getElementById('container')
+    const hello = document.createElement('p')
+    hello.textContent = 'Hello there!'
+    container?.children.length <  5 ? container?.appendChild(hello) : clearParagraphInterval()
+    
+}
 
-   
-
-// }
-
-// const interval = setInterval(hello, 2000)
-
+setTimeout(hello, 2000)
+setTimeout(addParagraph, 2000)
 // ! Part 3
 // onclick HTML 
 
-// const button = document.getElementById('button')
-// button.addEventListener("click", clear)
+const int = setInterval(addParagraph, 2000)
+const button = document.getElementById('clear')
+button?.addEventListener("click", clearParagraphInterval)
 
-// clear = () => {
-//     clearInterval(interval)
-// }
-// ! Clear Interval doesn't work
+
+clearParagraphInterval = () => {
+    clearInterval(int)
+}
+// ! Clear Interval not working
 // ? Check why
 
 // Exercise 2
+// let spaces = 0
+// interval;
 
-
-let spaces = 0
-let interval
-
-myMove = () => {
-    interval = setInterval(moveBox, 1)
-    return interval
-    }
+// myMove = () => {
+//     interval = setInterval(moveBox, 1)
+//     return interval
+//     }
     
-moveBox = () => {
-    spaces = spaces +  1
-    const box = document.getElementById('animate')
-    box.style.left = spaces + 'px'
+// moveBox = () => {
+//     spaces = spaces +  1
+//     const box = document.getElementById('animate')
+//     box.style.left = spaces + 'px'
     
-    if (spaces === 350) {
-        clearInterval(interval)
-    }
-}
+//     if (spaces === 350) {
+//         clearInterval(interval)
+//     }
+// }
 
-const container = document.getElementById('container')
+// const container = document.getElementById('container')
 
 
 
