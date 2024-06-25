d56e62ca4b0f4f803bf0561c97a7d71ceb59444c
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-25 19:45:26 +0300

    w 8 day 2 Daily Challenge

---
+++ DI_Bootcamp_stage_1/week_8/day2/Daily_Challenge/script.js
@@ -0,0 +1,33 @@
+// let client = "John";
+
+// const groceries = {
+//     fruits : ["pear", "apple", "banana"],
+//     vegetables: ["tomatoes", "cucumber", "salad"],
+//     totalPrice : "20$",
+//     other : {
+//         paid : true,
+//         meansOfPayment : ["cash", "creditCard"]
+//     }
+// }
+
+// const displayGroeries = () => {
+//     groceries["fruits"].forEach((item) => {
+//         console.log(item);
+//     })
+// }
+
+// const cloneGroceries = () => {
+//     let user = client;
+//     user = 'john'
+
+//     let shopping = groceries;
+//     console.log(shopping.totalPrice, groceries.totalPrice);
+
+//     shopping.totalPrice = `35$`;
+//     groceries.other.paid = false
+
+//     console.log(shopping.totalPrice, groceries.other.paid);
+
+// };
+// cloneGroceries();
+
