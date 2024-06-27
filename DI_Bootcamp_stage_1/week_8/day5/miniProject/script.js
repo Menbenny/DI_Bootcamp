903bc2053b7218991658afb47787da0c4e55883e
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-06-27 18:26:17 +0300

    miniProject - Random Quote Generator

---
+++ DI_Bootcamp_stage_1/week_8/day5/miniProject/script.js
@@ -0,0 +1,36 @@
+let quotes = [
+    {
+        id: 0,
+        author: 'Oscar',
+        quote: 'To live is the rarest thing in the world. Most people exist, that is all'
+    },
+    {
+        id: 1,
+        author: 'Emily Dickinson',
+        quote: 'That it will never come again is what makes life so sweet.'
+    },
+    {
+        id: 2,
+        author: '– George Eliot',
+        quote: 'It is never too late to be what you might have been.' 
+    },
+    {
+        id: 3,
+        author: '– Ralph Waldo Emerson',
+        quote: 'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.' 
+    },
+    {
+        id: 4,
+        author: '– Haruki Murakami',
+        quote: 'Pain is inevitable. Suffering is optional.' 
+    }
+]
+
+// console.log(quotes[1].author);
+function generateQuote() {
+    for (item in quotes) {
+        console.log(quotes[item]);
+    }
+}
+
+generateQuote()
