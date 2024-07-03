5d5f1048db85e4b013d860e2af0e670376972f16
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-03 13:23:32 +0300

    week 8 mini project - part 2

--- DI_Bootcamp_stage_1/week_8/day5/miniProject/script.js
+++ DI_Bootcamp_stage_1/week_8/day5/miniProject/script.js
@@ -1,37 +1,35 @@
 let quotes = [
     {
         id: 0,
-        author: 'Oscar',
+        author: '- Oscar',
         quote: 'To live is the rarest thing in the world. Most people exist, that is all'
     },
     {
         id: 1,
-        author: 'Emily Dickinson',
+        author: '- Emily Dickinson',
         quote: 'That it will never come again is what makes life so sweet.'
     },
     {
         id: 2,
-        author: '– George Eliot',
+        author: '- George Eliot',
         quote: 'It is never too late to be what you might have been.' 
     },
     {
         id: 3,
-        author: '– Ralph Waldo Emerson',
+        author: '- Ralph Waldo Emerson',
         quote: 'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.' 
     },
     {
         id: 4,
-        author: '– Haruki Murakami',
+        author: '- Haruki Murakami',
         quote: 'Pain is inevitable. Suffering is optional.' 
     }
 ]
 
 let lastQuoteIndex = -1
-// console.log(quotes[1].author);
+
 function generateQuote() {
-    // for (item in quotes) {
-    //     console.log(quotes[item]);
-    // }
+  
 
     let randomIndex;
     do {
@@ -43,11 +41,21 @@ function generateQuote() {
         const quoteSection = document.getElementById('quote-section');
         const randomQuote = generateQuote();
         quoteSection.innerHTML =  `<p>"${quotes[randomIndex].quote}"</p><p><em>- ${quotes[randomIndex].author}</em></p>`;
-    })
-    
-    // console.log(quotes[randomIndex])
-    
-    }
+    })    
+}
+
+document.getElementById('addQuoteForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const newQuoteText = document.getElementById('newQuote').value;
+    const newAuthorText = document.getElementById('newAuthor').value;
+    const newId = quotes.length;
+    const newQuote = {id: newId, author: newAuthorText, quote:newQuoteText};
+    quotes.push(newQuote);
+    document.getElementById('newQuote').value = '';
+    document.getElementById('newAuthor').value = '';
+});
+
+
     
 
 
