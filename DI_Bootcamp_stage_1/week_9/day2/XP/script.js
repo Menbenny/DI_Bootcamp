0421c8e90101dc50170e7ac6b29fbc7ceaecd303
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-03 23:09:48 +0300

    week 9 day 2

--- DI_Bootcamp_stage_1/week_9/day2/XP/script.js
+++ DI_Bootcamp_stage_1/week_9/day2/XP/script.js
@@ -11,17 +11,46 @@
 // displayStudentInfo({first: 'Vic', last: 'Maroth'})
 
 //Exercise 3
-const users = { user1: 18273, user2: 92833, user3: 90315 };
+// const users = { user1: 18273, user2: 92833, user3: 90315 };
 
-const arrUsers = Object.keys(users).reduce((arr, key) => {
-    arr.push(users[key]);
-    return arr;
-}, []);
+// const arrUsers = Object.keys(users).reduce((arr, key) => {
+//     arr.push(users[key]);
+//     return arr;
+// }, []);
 
-console.log(arrUsers);
+// console.log(arrUsers);
 
-const double = arrUsers.map((key, value) => value * 2)
-console.log(double);
+// const double = arrUsers.map(value => value * 2);
+// console.log(double);
 
 
 //Exercise 4
+// class Person {
+//     constructor(name) {
+//       this.name = name;
+//     }
+//   }
+  
+//   const member = new Person('John');
+//   console.log(typeof member);
+// ! Object
+
+
+// Exercise 5
+    // 4 SUCCESSFUL Extension
+// class Labrador extends Dog {
+//     constructor(name, size) {
+//       this.name = name;
+//       this.size = size;
+//     }
+//   };
+
+//exercise 6
+// 1. 
+//False
+//  error
+
+// 2. 
+// object2 = 4 //! object2 = object1 (object1.number = 4)
+// object3 = 4 //! object3 = object2 (object2 = object1 (object1.number = 4))
+// object4 = 5 //! object4 = {number: 5}
