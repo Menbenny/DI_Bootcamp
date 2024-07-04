d12dd17dfbb4bfd5446718bcd63be214e7413164
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-04 18:22:16 +0300

    miniProject draft

--- DI_Bootcamp_stage_1/week_9/day5/miniProject/script.js
+++ DI_Bootcamp_stage_1/week_9/day5/miniProject/script.js
@@ -86,3 +86,84 @@ class robot {
         this.email = 'Sincere@april.biz'
     }
 }
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Ervin Howell';
+      this.username = 'Antonette';
+      this.email = 'Shanna@melissa.tv'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Clementine Bauch';
+      this.username = 'Samantha';
+      this.email ='Nathan@yesenia.net'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Patricia Lebsack';
+      this.username =  'Karianne';
+      this.email = 'Julianne.OConner@kory.org'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Chelsey Dietrich';
+      this.username = 'Kamren';
+      this.email = 'Lucio_Hettinger@annie.ca'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name =  'Mrs. Dennis Schulist';
+      this.username = 'Leopoldo_Corkery';
+      this.email = 'Karley_Dach@jasper.info'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Kurtis Weissnat';
+      this.username = 'Elwyn.Skiles';
+      this.email = 'Telly.Hoeger@billy.biz'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name =  'Nicholas Runolfsdottir V';
+      this.username = 'Maxime_Nienow';
+      this.email =  'Sherwood@rosamond.me'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Glenna Reichert';
+      this.username = 'Delphine';
+      this.email = 'Chaim_McDermott@dana.io'
+  }
+}
+
+class robot {
+  constructor (id, name, username, email) {
+      this.id = id;
+      this.name = 'Clementina DuBuque';
+      this.username = 'Moriah.Stanton';
+      this.email ='Rey.Padberg@karina.biz'
+  }
+}
