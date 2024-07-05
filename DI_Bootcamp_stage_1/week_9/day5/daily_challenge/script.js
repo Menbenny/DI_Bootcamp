254e67d46885d52a42301cb37190bb0308de70a4
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-05 15:53:38 +0300

    daily-challenge

---
+++ DI_Bootcamp_stage_1/week_9/day5/daily_challenge/script.js
@@ -0,0 +1,12 @@
+function anagramChecker(string, anagram) {
+    for (let i = 0; i < string.length; i++){
+
+        console.log(string[i]);
+    //     for (let x = 0; x >= anagram.length; x++) {
+    //     }
+    //     console.log(i, x);
+    }
+}
+// const string = 'care';
+// anagram = 'race'
+anagramChecker('string', 'anagram')
