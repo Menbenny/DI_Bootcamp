d3a35f71ce3cf3dd3c8ad55362d3e93a3e10c7dc
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-10 20:52:25 +0300

    daily challenge

--- DI_Bootcamp_stage_1/week_10/day4/daily_challenge/script.js
+++ DI_Bootcamp_stage_1/week_10/day4/daily_challenge/script.js
@@ -1,33 +1,61 @@
 const input = document.querySelector("input")
-const theValue = input.value.toLowerCase()
-const button = document.querySelector("button")
-
-button.addEventListener("click", () => {
-    event.preventDefault();
-    console.log(theValue);
-});
-
+// const theValue = input.value.toLowerCase()
+const displayDiv = document.getElementsByClassName('display')
+// const submitButton = document.querySelector("button")
+const deleteAllButton = document.getElementById('deleteAllButton')
 const API_Key = `hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`
-const url = `https://api.giphy.com/v1/gifs/random/${API_Key}`;
+const url = `https://api.giphy.com/v1/gifs/random/?api_key=${API_Key}&tag=${category}`;
 const options = {
     method: "GET",
-    headers: {"Content-type": "application/json"},
-    body: JSON.stringify(`/${theValue}`)
+    headers: {"Content-type": "application/json"}
+    
 }
 
-const fetchRandomGIF = async() => {
+
+button.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const category = input.value.trim().toLowerCase()
+    if (category) {
+        const gif_URL = await fetchRandomGIF(category);
+        if (gif_URL) {
+            appendGif(gif_URL)
+        }
+    }
+    // console.log(theValue);
+});
+
+deleteAllButton.addEventListener('click', () => {
+    displayDiv.innerHTML = '';
+})
+
+
+const fetchRandomGIF = async(category) => {
     try {
         let response = await fetch(url, options);
-        let result = await response.json();
+        let result = await response.json(); 
+        return result.result.images.fixed_height.url;
         // console.log(result);
-        if (theValue === 'sun') {
-            const sunCategory = document.createElement('div');
-            sunCategory.innerHTML = `<h2>Category Sun</h2>`;
-            sunCategory.appendChild(result)
-        }
     } catch (error) {
-        console.log(error.message);
+        console.log(`Error fetching GIF`, error);
     }
 }
 
-fetchRandomGIF()
+appendGif = () => {
+    const gifItem = document.createElement('div');
+    gifItem.classList.add(`gif-item`);
+
+    const img = document.createElement(`img`);
+    img.src = url;
+    gifItem.appendChild(img);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = `DELETE`;
+    deleteButton.addEventListener('click', () => {
+        gifItem.remove();
+    });
+    gifItem.appendChild(deleteButton);
+
+    displayDiv.appendChild(gifItem);
+}
+
+// fetchRandomGIF()
