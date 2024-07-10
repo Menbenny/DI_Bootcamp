95d06d6e0c9e64c3d04315513348c71a8d538a7d
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-10 17:36:10 +0300

    dailychallenge

--- DI_Bootcamp_stage_1/week_10/day4/daily_challenge/script.js
+++ DI_Bootcamp_stage_1/week_10/day4/daily_challenge/script.js
@@ -1,5 +1,5 @@
 const input = document.querySelector("input")
-const theValue = input.value
+const theValue = input.value.toLowerCase()
 const button = document.querySelector("button")
 
 button.addEventListener("click", () => {
@@ -11,15 +11,20 @@ const API_Key = `hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`
 const url = `https://api.giphy.com/v1/gifs/random/${API_Key}`;
 const options = {
     method: "GET",
-    body: JSON.stringify({theValue}),
-    headers: "Content-type", "application/json"
+    headers: {"Content-type": "application/json"},
+    body: JSON.stringify(`/${theValue}`)
 }
 
 const fetchRandomGIF = async() => {
     try {
         let response = await fetch(url, options);
         let result = await response.json();
-        console.log(result);
+        // console.log(result);
+        if (theValue === 'sun') {
+            const sunCategory = document.createElement('div');
+            sunCategory.innerHTML = `<h2>Category Sun</h2>`;
+            sunCategory.appendChild(result)
+        }
     } catch (error) {
         console.log(error.message);
     }
