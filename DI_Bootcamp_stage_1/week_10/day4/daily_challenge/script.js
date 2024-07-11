d519335c8297c215f42054638ffb788ef51bd4da
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-11 09:04:01 +0300

    daily challenge - code fixes

--- DI_Bootcamp_stage_1/week_10/day4/daily_challenge/script.js
+++ DI_Bootcamp_stage_1/week_10/day4/daily_challenge/script.js
@@ -1,17 +1,20 @@
-const input = document.getElementById("categoryInput");
-const category = input.value.trim().toLowerCase()
-const gifForm = document.getElementById('gifForm');
-
-const deleteAllButton = document.getElementById('deleteAllButton')
 const apiKey = `hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My`
-const searchButton = document.getElementById('searchButton')
-const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${category}`;
+const gifForm = document.getElementById('gifForm');
+const input = document.getElementById("categoryInput");
+const deleteAllButton = document.getElementById('deleteAllButton')
+const displayDiv = document.getElementById('display');
 
-searchButton.addEventListener("submit", async (event) => {
+
+const searchButton = document.getElementById('searchButton')
+
+
+gifForm.addEventListener("submit", async (event) => {
     event.preventDefault();
-    
-    if (category) {
-        const gif_URL = await fetchRandomGIF(category);
+    const inputCategory = input.value.trim();
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=${inputCategory}`;
+
+    if (inputCategory) {
+        const gif_URL = await fetchRandomGIF(url);
         if (gif_URL) {
             appendGif(gif_URL)
         }
@@ -24,7 +27,9 @@ deleteAllButton.addEventListener('click', () => {
 })
 
 
-const fetchRandomGIF = async(category) => {
+const fetchRandomGIF = async(url) => {
+    
+    
     
     const options = {
         method: "GET",
@@ -35,14 +40,15 @@ const fetchRandomGIF = async(category) => {
     try {
         let response = await fetch(url, options);
         let result = await response.json(); 
-        return result.data.images.fixed_height.url;
-        // console.log(result);
+        return result.data.images.original.url;
+        // return console.log(result);;
+        
     } catch (error) {
         console.log(`Error fetching GIF`, error);
     }
 }
 
-appendGif = (url) => {
+const appendGif = (url) => {
     const gifItem = document.createElement('div');
     gifItem.classList.add(`gif-item`);
 
@@ -55,10 +61,9 @@ appendGif = (url) => {
     deleteButton.addEventListener('click', () => {
         gifItem.remove();
     });
-    gifItem.appendChild(deleteButton);
-    const displayDiv = document.getElementById('display').appendChild(gifItem);
-    // displayDiv.appendChild(gifItem);
+   gifItem.appendChild(deleteButton);
+
+   displayDiv.appendChild(gifItem);
 }
 
 // appendGif(url)
-fetchRandomGIF(category)
