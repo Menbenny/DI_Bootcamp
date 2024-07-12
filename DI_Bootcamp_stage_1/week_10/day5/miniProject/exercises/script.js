3aad75d6f9f12493e350e10c36c93c78f0ff8b87
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-12 20:02:20 +0300

    styling - functionality incomplete

--- DI_Bootcamp_stage_1/week_10/day5/miniProject/exercises/script.js
+++ DI_Bootcamp_stage_1/week_10/day5/miniProject/exercises/script.js
@@ -1,33 +1,29 @@
 const findButton = document.getElementById('findButton');
 const displayDiv = document.getElementById('display');
-// console.log(randomNumber);
 
-
-
-// ? with each click - run a loop which iterates over the values of the API array in the URL:`https://www.swapi.tech
 findButton.addEventListener('click', (event) => {
     event.preventDefault();
+
     let randomNumber = Math.floor(Math.random() * 83 ) + 1;
-    let fetchURL =  `https://www.swapi.tech/api/people/${randomNumber}`
+    let fetchURL =  `https://www.swapi.tech/api/people/${randomNumber}`;
+
     findSomeoneFunction = async(fetchURL) => {
         try {
             let response = await fetch(fetchURL);
             let data = await response.json()
             const character = data.result.properties
 
-            console.log(character);
-
+            
             const worldUrl = character.homeworld
             let res = await fetch(worldUrl)
             let worldData = await res.json()
             const homeWorldname = worldData.result.properties.name
-            console.log(`home: ${homeWorldname}`);
 
             displayDiv.innerHTML = '';
 
-            const nameTag = document.createElement('h1');
+            const nameTag = document.createElement('h4');
             nameTag.textContent = `${character.name}`;
-            const height = document.createElement('h4');
+            const height = document.createElement('p');
             height.textContent = `Height: ${character.height}`;
             const gender = document.createElement(`p`);
             gender.textContent = `Gender: ${character.gender}`
@@ -41,9 +37,7 @@ findButton.addEventListener('click', (event) => {
             displayDiv.appendChild(gender)
             displayDiv.appendChild(birthYear)
             displayDiv.appendChild(homeWorldTag)
-
             
-
             return console.log(displayDiv);
 
         } catch (error) {
