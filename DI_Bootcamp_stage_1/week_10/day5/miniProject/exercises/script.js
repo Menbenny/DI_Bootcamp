c12c20750cb265cd740683ab2a7720a63dcdcb11
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-12 19:39:23 +0300

    start wars - fetch data complete

--- DI_Bootcamp_stage_1/week_10/day5/miniProject/exercises/script.js
+++ DI_Bootcamp_stage_1/week_10/day5/miniProject/exercises/script.js
@@ -3,17 +3,26 @@ const displayDiv = document.getElementById('display');
 // console.log(randomNumber);
 
 
+
 // ? with each click - run a loop which iterates over the values of the API array in the URL:`https://www.swapi.tech
 findButton.addEventListener('click', (event) => {
     event.preventDefault();
-    let randomNumber = Math.floor(Math.random() * 83 );
+    let randomNumber = Math.floor(Math.random() * 83 ) + 1;
     let fetchURL =  `https://www.swapi.tech/api/people/${randomNumber}`
     findSomeoneFunction = async(fetchURL) => {
         try {
             let response = await fetch(fetchURL);
             let data = await response.json()
             const character = data.result.properties
-console.log(character);
+
+            console.log(character);
+
+            const worldUrl = character.homeworld
+            let res = await fetch(worldUrl)
+            let worldData = await res.json()
+            const homeWorldname = worldData.result.properties.name
+            console.log(`home: ${homeWorldname}`);
+
             displayDiv.innerHTML = '';
 
             const nameTag = document.createElement('h1');
@@ -24,64 +33,28 @@ console.log(character);
             gender.textContent = `Gender: ${character.gender}`
             const birthYear = document.createElement(`p`);
             birthYear.textContent = `Birth year: ${character.birth_year}`;
-            const homeWorld = document.createElement(`p`);
-            homeWorld.textContent = `Home world: ${character.homeworld}`
+            const homeWorldTag = document.createElement(`p`);
+            homeWorldTag.textContent = `Home world: ${homeWorldname}`
 
             displayDiv.appendChild(nameTag)
             displayDiv.appendChild(height)
             displayDiv.appendChild(gender)
             displayDiv.appendChild(birthYear)
-            displayDiv.appendChild(homeWorld)
+            displayDiv.appendChild(homeWorldTag)
+
+            
 
             return console.log(displayDiv);
 
         } catch (error) {
             console.log(`fetch error`);
         }
-        
+        homeWorldFunc(worldUrl)
     }
     
     findSomeoneFunction(fetchURL)
-    // console.log(character.height);
+});
 
-    // DOM
-    /*
-    name
-    height
-    geneder
-    birthyears
-    home land
-    */
-
-
-})
-
-
-
-
-// Get info from API and catch the errors 
-// get random poeple in the api between the number 1 and 88
-// i.e randomNumber variable & apiURL
-
-// ajax xhr = new XMLHttpRequest();
-/*
-ajax syntax:
-    open
-    responseType
-    send()
-    onload = function(){
-            if(//! set a status value conditional -- i.e status != 200)
-                updateInfoError();
-
-                console.log(`there was an error`)
-        }else {
-         updateInfo(xhr.response);
-        }
-
-    function updateInfo(){
-        
-    }
-*/
 
 
 
