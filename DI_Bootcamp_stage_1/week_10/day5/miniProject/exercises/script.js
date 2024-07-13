1d837fc7969d666613a6bbf4a6a0f0dcfbbf8042
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-13 15:16:13 +0300

    StarWars - fetch, loading functions & styling comlplete

--- DI_Bootcamp_stage_1/week_10/day5/miniProject/exercises/script.js
+++ DI_Bootcamp_stage_1/week_10/day5/miniProject/exercises/script.js
@@ -1,49 +1,47 @@
 const findButton = document.getElementById('findButton');
 const displayDiv = document.getElementById('display');
 
+
+
 findButton.addEventListener('click', (event) => {
     event.preventDefault();
 
-    let randomNumber = Math.floor(Math.random() * 83 ) + 1;
-    let fetchURL =  `https://www.swapi.tech/api/people/${randomNumber}`;
+    loaderIcon();
+    
+    const randomNumber = Math.floor(Math.random() * 83 ) + 1;
+    const fetchURL =  `https://www.swapi.tech/api/people/${randomNumber}`;
 
-    findSomeoneFunction = async(fetchURL) => {
+    const findSomeoneFunction = async(fetchURL) => {
+        
         try {
-            let response = await fetch(fetchURL);
-            if (!response.ok) {
-                throw new Error (`Oh no! That person isn't available`)
-            }
-            let data = await response.json()
-            const character = data.result.properties
-            
-            
+            const response = await fetch(fetchURL);
+            const data = await response.json()
+            const character = data.result.properties 
             const worldUrl = character.homeworld
-            let res = await fetch(worldUrl);
-            if (!res.ok) {
-                throw new Error (`Home world not found`)
-            }
-            let worldData = await res.json()
+            
+            const res = await fetch(worldUrl);
+            const worldData = await res.json()
             const homeWorldname = worldData.result.properties.name
 
             displayDiv.innerHTML = '';
 
             const nameTag = document.createElement('h4');
-            nameTag.textContent = `${character.name}`;
             const height = document.createElement('p');
-            height.textContent = `Height: ${character.height}`;
             const gender = document.createElement(`p`);
-            gender.textContent = `Gender: ${character.gender}`
             const birthYear = document.createElement(`p`);
-            birthYear.textContent = `Birth year: ${character.birth_year}`;
             const homeWorldTag = document.createElement(`p`);
-            homeWorldTag.textContent = `Home world: ${homeWorldname}`
+
+            nameTag.textContent = `${character.name}`;
+            height.textContent = `Height: ${character.height}`;
+            gender.textContent = `Gender: ${character.gender}`;
+            birthYear.textContent = `Birth year: ${character.birth_year}`;
+            homeWorldTag.textContent = `Home world: ${homeWorldname}`;
 
             displayDiv.appendChild(nameTag)
             displayDiv.appendChild(height)
             displayDiv.appendChild(gender)
             displayDiv.appendChild(birthYear)
             displayDiv.appendChild(homeWorldTag)
-            return console.log(displayDiv);
 
         } catch (error) {
             console.log(`fetch error`);
@@ -52,6 +50,10 @@ findButton.addEventListener('click', (event) => {
     findSomeoneFunction(fetchURL)
 });
 
+const loaderIcon = () => {
+    displayDiv.innerHTML = `  <i class="fa-solid fa-spinner fa-spin-pulse"></i> <p>Loading...</p>`;
+  
+}
 
 
 
