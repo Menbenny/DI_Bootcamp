6f7d2cf1e9f0f581f890039ba69ae3e1480c8770
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-13 23:03:31 +0300

    js logic layout fixes - broken code

--- DI_Bootcamp_stage_1/week_10/day5/miniProject/daily_challenge/script.js
+++ DI_Bootcamp_stage_1/week_10/day5/miniProject/daily_challenge/script.js
@@ -1,106 +1,95 @@
 const api_key = `b17e4e753adbf4cc2d5e4a72`;
-// let currencies = '';
-
-
-// const option = document.querySelector('option');
-const select = document.getElementById('currencies')
-// const primaryCurrency = select.value;
-
-// select.addEventListener('change', () => {   
-//     const optionValue = select.value
-//     console.log(optionValue);
-// })
-
-
-
-// ! currency (FROM) URL
-const primaryURL = `https://v6.exchangerate-api.com/v6/${api_key}/latest/USD`
-
-fetchPrimaryCurrency = async(primaryURL) => {
-    try {
-        let response = await fetch(primaryURL);
-        let data = await response.json();
-
-
-        console.log(data);
-
-
-
-    } catch (error) {
-        console.log(error, `data not found!`);
-    }
-}
-
-fetchPrimaryCurrency(primaryURL)
-
-// ! currency (TO) URL
-const secondaryURL = `https://v6.exchangerate-api.com/v6/${api_key}/latest/USD`
-
-// fetchSecondaryCurrency = async(secondaryURL) => {
-//     try {
-//         let secondResponse = await fetch(secondaryURL);
-//         let secondData = await secondResponse.json();
-
-
-
-//         console.log(secondData.conversion_rates.option.value);
-//     } catch (error) {
-//         console.log(error, `data not found!`);
-//     }
-// }
-
-// fetchSecondaryCurrency(secondaryURL)
-
-
+const selectFrom = document.getElementById('fromCurrencies')
+const selectTo = document.getElementById('ToCurrencies')
 const supportedCurrencies = `https://v6.exchangerate-api.com/v6/${api_key}/codes`
 
-firstListOfCurrencies = async(supportedCurrencies) => {
+const firstListOfCurrencies = async(supportedCurrencies) => {
     try {
         let response = await fetch(supportedCurrencies);
         let supportedCurrenciesData = await response.json();
 
-        // console.log(supportedCurrenciesData.supported_codes[2]);
-        
-
         for (let x = 0; x < supportedCurrenciesData.supported_codes.length; x ++) {
             const currenciesArrays = supportedCurrenciesData.supported_codes[x];
+            const currenciesCode = currenciesArrays[0];
+            const currencyName = currenciesArrays[1];
+            
+            const option = document.createElement('option')
+            option.value = currenciesCode
+            option.textContent = `${currenciesCode} - ${currencyName}`
+            selectFrom.appendChild(option);
+        }
+
+    } catch (error) {
+        console.log(error, `No data found`);
+    } 
+};
+
+const secondListOfCurrencies = async(supportedCurrencies) => {
+    try {
+        let response = await fetch(supportedCurrencies);
+        let supportedCurrenciesData = await response.json();
+
+        for (let y = 0; y < supportedCurrenciesData.supported_codes.length; y ++) {
+            const currenciesArrays = supportedCurrenciesData.supported_codes[y];
             const currenciesCode = currenciesArrays[0];
             const currencyName = currenciesArrays[1];
 
             const option = document.createElement('option')
             option.value = currenciesCode
             option.textContent = `${currenciesCode} - ${currencyName}`
-          
-            select.appendChild(option);
-        }
         
-
-        // let currenciesCodes = JSON.parse(supportedCurrenciesData.supported_codes)
+            selectTo.appendChild(option);
+            
+        }
 
     } catch (error) {
         console.log(error, `No data found`);
     }
 }
+
+const fetchPrimaryCurrency = async(primaryURL, selectedCurrency) => {
+    try {
+        let response = await fetch(primaryURL);
+        let data = await response.json();
+        let conversionRate = data.conversion_rates
+
+        console.log(selectedCurrency);
+        console.log(conversionRate[selectedCurrency]);
+    } catch (error) {
+        console.log(error, `data not found!`);
+    }
+}
+
+const fetchSecondaryCurrency = async(secondaryURL, selectedCurrency) => {
+    try {
+        let response = await fetch(secondaryURL);
+        let data = await response.json();
+        let conversionRate = data.conversion_rates
+
+        console.log(selectedCurrency);
+        console.log(conversionRate[selectedCurrency]);
+    } catch (error) {
+        console.log(error, `data not found!`);
+    }
+}
+
+selectFrom.addEventListener('change', () => {   
+    const selectedCurrency = selectFrom.value;
+    const primaryURL = `https://v6.exchangerate-api.com/v6/${api_key}/latest/${selectedCurrency}`    
+    fetchPrimaryCurrency(primaryURL)
+})
+
+
+
+selectTo.addEventListener('change', () => {
+    const selectedCurrency = selectTo.value
+    const secondaryURL = `https://v6.exchangerate-api.com/v6/${api_key}/latest/${selectedCurrency}`
+    fetchSecondaryCurrency(secondaryURL)
+
+})
+
 firstListOfCurrencies(supportedCurrencies)
+secondListOfCurrencies(supportedCurrencies)
 
-// converter = async(primaryURL) => {
 
-//     try {
-
-//         let response = await fetch(primaryURL);
-//         let data = await response.json();
-
-//         // let currency = data.base_code;
-
-//         console.log(data);
-//         // if (currencies === `USD`) {
-//         //     
-//         // }
-        
-//     } catch (error) {
-//         console.log(error, `Data not found`);
-//     }
-// }
-
-// converter(primaryURL)
 
