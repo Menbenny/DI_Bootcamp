feeccaf75452af9ce89cd37b1a501e6f637684ab
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-15 13:47:43 +0300

    week11 - day2 - exe3

---
+++ DI_Bootcamp_stage_1/week_11/day2/XP/exe3/app.js
@@ -0,0 +1,7 @@
+// ! Not setting an extension - NodeJS with append (`.js`/ `.json`/ `.txt`) to the file being required
+
+const readFile = require(`./fileManager.js`)
+const writeFile =  require(`./fileManager.js`)
+
+readFile()
+writeFile()
