e5a5c2c0b4d0958c412b3cde53a42e5944e99789
Binyamin_Marothi <menacbinyamin@gmail.com>
2024-07-21 00:46:15 +0300

    week 11 - day4 - exercise 2

---
+++ DI_Bootcamp_stage_1/week11/day4/XP/book-api/app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const books = [
+    {
+        id: 1,
+        title: "Book1",
+        author: "author1",
+        publishedYear: 2001
+    },
+    {
+        id: 2,
+        title: "Book2",
+        author: "author2",
+        publishedYear: 2002
+    },
+    {
+        id: 3,
+        title: "Book3",
+        author: "author3",
+        publishedYear: 2003
+    },
+]
+
+app.use(express.json())
+// app.get('/api/books', (req, res)=>{
+//     res.status(200).json(books)
+// })
+
+// app.get('/api/books', (req, res)=>{
+//     const id = Number(req.query.id)
+//     const book = books.find((book)=> book.id===id)
+
+//     if(!book){
+//         res.status(404).send('Book not found')
+//     }
+//     res.status(200).json(book)
+// })
+
+app.post('/api/books', (req, res)=>{
+    const id = Number(req.query.id);
+    const index = books.findIndex((book)=>book.id ===id)
+
+    if(index === -1){
+        res.status(404).send('Book not found')
+    }
+
+    const newBook = {
+        id: books[index].id,
+        title: req.body.title,
+        author: req.body.author,
+        publishedYear: req.body.publishedYear
+    }
+
+    books.push(newBook);
+    res.status(201).json({msg: "Book created"})
+})
+
+const port = 5000;
+app.listen(port, ()=>{
+    console.log(chalk.bgCyan(`Listening on port: ${port}`));
+})
