0054278ad02f62de31ca2e11663dd3b47c9b4806
Menac_B <menacbinyamin@gmail.com>
2024-07-21 11:55:23 +0300

    xp-gold - exercise 2 - incomplete

--- DI_Bootcamp_stage_1/week11/day2/XP_Gold/axios-example/fetch-data.js
+++ DI_Bootcamp_stage_1/week11/day2/XP_Gold/axios-example/fetch-data.js
@@ -1,2 +1,22 @@
-import axios from 'axios';
+const axios = require('axios');
+const http = require('http');
+const chalk = require('chalk');
+const url = 'https://jsonplaceholder.typicode.com/posts'
+
+const server = http.createServer('/posts', (req, res)=>{
+    
+    // ? which params are solved
+    // ? is solution async or sync
+
+    fetch(url)
+        .then((req)=>{
+            console.log(req);
+        })
+})
+
+const port = 5000
+server.listen(port, ()=>{
+    console.log(chalk.cyan(`listening on port: ${port}`));
+})
+
 
