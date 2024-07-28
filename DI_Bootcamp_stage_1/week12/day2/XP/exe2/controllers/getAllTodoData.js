7b62ec065e15453c3a3f7b71d2626ecddd1588c7
Menac_B <menacbinyamin@gmail.com>
2024-07-28 21:56:32 +0300

    exe2

--- DI_Bootcamp_stage_1/week12/day2/XP/exe2/controllers/getAllTodoData.js
+++ DI_Bootcamp_stage_1/week12/day2/XP/exe2/controllers/getAllTodoData.js
@@ -1,8 +1,24 @@
+const { todoData } = require('../data/todoData')
 const getAllTodoData = (req, res) => {
 //    const { id } = req.params;
    res.json()
 }
 
 const updateTodo = (req, res) => {
-    
+    const {task, time, taskHandler} = req.body
+
+    const newEntry = {
+        ...req.body,
+        task: task,
+        time: time,
+        taskHandler: taskHandler
+    }
+
+    todoData.push(newEntry)
+    res.json()
+}
+
+module.exports = {
+    updateTodo,
+    getAllTodoData
 }
