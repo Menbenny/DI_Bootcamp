195efd0876fca610f69a68df6edaa20af233ea41
Menac_B <menacbinyamin@gmail.com>
2024-07-28 22:01:15 +0300

    exe2

--- DI_Bootcamp_stage_1/week12/day2/XP/exe2/controllers/getAllTodoData.js
+++ DI_Bootcamp_stage_1/week12/day2/XP/exe2/controllers/getAllTodoData.js
@@ -1,9 +1,13 @@
 const { todoData } = require('../data/todoData')
+
+
+// ! GET
 const getAllTodoData = (req, res) => {
 //    const { id } = req.params;
    res.json()
 }
 
+//! PUT
 const updateTodo = (req, res) => {
     const {task, time, taskHandler} = req.body
 
@@ -18,7 +22,21 @@ const updateTodo = (req, res) => {
     res.json()
 }
 
+// ! Delete
+const todoDelete = (req, res) => {
+    const { id } = req.body
+    const index = todoData.findInex((item)=> item.id == id)
+
+    if(!index) {
+        res.status(404).send(`Product not found`)
+    }
+
+    todoData.splice(index, 1)
+    res.json()
+}
+
 module.exports = {
     updateTodo,
-    getAllTodoData
+    getAllTodoData,
+    todoDelete
 }
