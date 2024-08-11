48410b0c21f2dac5ad76c1ad412e8f58cca62c4c
Menac_B <menacbinyamin@gmail.com>
2024-08-11 08:47:15 +0300

    react-update

---
+++ DI_Bootcamp_stage_1/week13/Vite/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import users from './assets/users.json'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+console.log(users);
+
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return <></>
+  
+}
+
+export default App
