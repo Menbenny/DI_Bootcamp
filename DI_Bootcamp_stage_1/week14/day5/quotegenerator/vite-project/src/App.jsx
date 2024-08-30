0caebc2b0a740f39d9d3df2bc65f333c8ee4e4f8
Menac_B <menacbinyamin@gmail.com>
2024-08-30 16:06:32 +0300

    miniProject - week14 - randomQutoe generator

---
+++ DI_Bootcamp_stage_1/week14/day5/quotegenerator/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Quotes from './components/Quotes'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
