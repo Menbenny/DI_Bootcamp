85c683d178cd6d4a5fce0084d23fc1838b85f896
Menac_B <menacbinyamin@gmail.com>
2024-09-20 17:06:58 +0300

    w12d4

--- DI_Bootcamp_stage_1/week12/day4/XP/exe2/book-api/app.js
+++ DI_Bootcamp_stage_1/week12/day4/XP/exe2/book-api/app.js
@@ -9,3 +9,36 @@ let books = [
   { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', publishedYear: 1960 },
   { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publishedYear: 1925 },
 ];
+
+app.get('/api/books', (req, res)=> {
+    res.status(200).json(books);
+});
+
+app.get('/api/books/:bookId', (req,res)=> {
+    const bookId = parseInt(req.params.bookId);
+    const book = books.find((b) => b.id === bookId);
+
+    if(!book) {
+        return res.status(404).json({ message: 'Book not found'});
+    }
+
+    res.status(200).json(book);
+});
+
+app.post('/api/books', (req, res)=> {
+    const { title, author, publishedYear} = req.body;
+
+    if(!title || !author || !publishedYear)  {
+        return res.status(400).json({message : 'Title, author and publish year are required'})
+    }
+
+    const newBook = {
+        id: books.length+1,
+        title,
+        author,
+        publishedYear
+    };
+    
+    books.push(newBook);
+    res.status(201).json(newBook);
+})
