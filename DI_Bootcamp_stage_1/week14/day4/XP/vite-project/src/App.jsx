83ac8aba0388fb20f0135a37fb8bcd0e3ee2dbe7
Menac_B <menacbinyamin@gmail.com>
2024-09-27 17:24:46 +0300

    w14

--- DI_Bootcamp_stage_1/week14/day4/XP/vite-project/src/App.jsx
+++ DI_Bootcamp_stage_1/week14/day4/XP/vite-project/src/App.jsx
@@ -1,60 +1,51 @@
 import { useState } from 'react'
 import './App.css'
-import ErrorBoundary from './ErrorBoundary'
+import ErrorBoundary from './component/ErrorBoundary'
+import BuggyCounter from './component/buggyCounter'
+import LifecycleDemo from './component/LifecycleDemo'
 
-class BuggyCounter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {counter: 0}
-  }
+const App = () => {
+  const [simulation, setSimulation] = useState('1');
 
-  handleClick = () => {
-    this,setState(({ counter }) => ({
-      counter: counter + 1
-    }))
+  const handleSimulationChange = (e) => {
+    setSimulation(e.target.value);
   };
 
-  render() {
-    if (this.state.counter === 5) {
-      throw new Error('I crashed!')
+  const renderSimulation = () => {
+    switch (simulation) {
+      case '1':
+        return (
+          <div>
+            <h2>Simulation 1: One ErrorBoundary wrapping two BuggyCounters</h2>
+            <ErrorBoundary>
+              <BuggyCounter />
+              <BuggyCounter />
+            </ErrorBoundary>
+          </div>
+        );
+      case '2':
+        return (
+          <div>
+            <h2>Simluation 2 : Each BuggyCounter has its own ErrorBoundary</h2>
+            <ErrorBoundary>
+              <BuggyCounter />
+            </ErrorBoundary>
+            <ErrorBoundary>
+              <BuggyCounter />
+            </ErrorBoundary>
+          </div>
+        );
+      case '3':
+        return (
+          <div>
+            <h2>Simulation 3: BuggyCounter without ErrorBoundary</h2>
+            <BuggyCounter />
+          </div>
+        );
+      case '4':
+        return <LifecycleDemo/>
     }
-    return (
-      <h1 onClick={this.handleClick}>
-        {this.state.counter}
-      </h1>
-    );
   }
-}
-
-class App extends
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
 export default App
--- DI_Bootcamp_stage_1/week14/day4/XP/vite-project/src/component/buggyCounter.jsx
+++ DI_Bootcamp_stage_1/week14/day4/XP/vite-project/src/component/buggyCounter.jsx
@@ -1,3 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class BuggyCounter extends
+const BuggyCounter = () => {
+    const [counter, setCounter] = useState(0);
+
+    const handleClick = () => {
+        setCounter((prevCounter) => prevCounter + 1)
+    };
+
+    if(counter === 5 ) {
+        throw new Error('I crashed!')
+    }
+
+    return (
+        <h1
+            onClick={handleClick}
+            style={{ cursor: 'pointer', userSelect: 'none', margin: '10px'}}
+            title='Click to increment'
+        >
+            {counter}
+        </h1>
+    );
+};
+
+export default BuggyCounter;
+
